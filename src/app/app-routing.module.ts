eeeb1d596a83b98bacb2dbd802499394e6a1b0ea
lukemcanelly <mcanellyluke@gmail.com>
2018-11-13 03:32:28 -0600

    Gave functionality to header buttons.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LinksComponent } from './components/links/links.component';
+import { ScheduleComponent } from './components/schedule/schedule.component'
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'links', component: LinksComponent},
+  { path: 'schedule', component: ScheduleComponent}
+
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { LinksComponent } from './components/links/links.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LinksComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
